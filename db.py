b200366c9f6edc9c66d817803b3fdf4d58295a01
Karl Mathias Moberg <km@kmoberg.no>
2025-02-06 18:14:13 +0100

    Store the database properly in /data/

--- db.py
+++ db.py
@@ -11,7 +11,7 @@ def init_db_uri(app):
     If you want a different path or a PostgreSQL URI, adjust here.
     """
     base_dir = os.path.abspath(os.path.dirname(__file__))
-    db_path = os.path.join(base_dir, "aircraft.db")
+    db_path = os.path.join(base_dir, "data/aircraft.db")
     app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     return app
